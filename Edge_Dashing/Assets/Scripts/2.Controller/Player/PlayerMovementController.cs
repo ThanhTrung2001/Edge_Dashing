bf1cc9db6f248f3ca3ae67c3cc66b2b9b25c27cc
V.Kohaku <thanhtrungtran13@gmail.com>
2023-04-16 20:37:20 +0700

    update movement

--- Edge_Dashing/Assets/Scripts/2.Controller/Player/PlayerMovementController.cs
+++ Edge_Dashing/Assets/Scripts/2.Controller/Player/PlayerMovementController.cs
@@ -28,14 +28,14 @@ public class PlayerMovementController : MonoBehaviour
   {
     if(Input.GetAxis("Horizontal") > 0 )
     {
-      rb.AddForce(manager.model.horizonSpeed*Time.deltaTime,0,0);
+      rb.AddForce(manager.model.horizonSpeed*Time.deltaTime,0,0, ForceMode.VelocityChange);
     }
   }
   void MovingRight()
   {
     if(Input.GetAxis("Horizontal") < 0 )
     {
-      rb.AddForce(-manager.model.horizonSpeed*Time.deltaTime,0,0);
+      rb.AddForce(-manager.model.horizonSpeed*Time.deltaTime,0,0, ForceMode.VelocityChange);
     }
   }
 
@@ -45,7 +45,7 @@ public class PlayerMovementController : MonoBehaviour
     {
       if(Input.GetAxis("Vertical") > 0  || Input.GetKey("space")) // 
       { 
-        rb.AddForce(0,manager.model.jumpForce*Time.deltaTime,0);
+        rb.AddForce(0,manager.model.jumpForce*Time.deltaTime,0, ForceMode.VelocityChange);
       }
     }
   }
